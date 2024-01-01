3e94dd0eacc32135e0ee8121d5f6a62ea6518264
aak114 <77752924+aak114@users.noreply.github.com>
2024-01-01 06:56:12 +0100

    Account bg white

--- screens/Account.tsx
+++ screens/Account.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Account = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='bg-white h-full'>
 
         {/* Header */}
         <View className='p-5 flex flex-row'>
